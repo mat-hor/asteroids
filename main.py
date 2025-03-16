0c25bc0716bcce18ed4388e454e3d2286d3a4653
mat-hor <matt.hor.dev@gmail.com>
2025-03-16 09:58:07 +0100

    Added constan values and imported to main

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+import constants
 
 def main():
     print("Starting Asteroids!")
+    print(constants.SCREEN_WIDTH)
+    print(constants.SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()
